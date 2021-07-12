1db2731f85e81df5b4e20cb75ae3f582bb33c6de
luis carlos <luizcruz0809@gmail.com>
2021-07-12 17:15:01 -0300

    add 06

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const oL = () => {
+  return document.querySelector('.cart__items');
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -19,15 +23,16 @@ function createCustomElement(element, className, innerText) {
   function saveLocStorCart() { // atualizar o local Storage quando clica para remover e quando requisitar 
     const lis = document.querySelectorAll('.cart__item');
     const arrayLis = [];
-  
-    for (let index = 0; index < lis.length; index += 1) {
-      arrayLis.push(lis[index].innerText);
+
+    if (lis !== null && lis.length > 0) {
+      for (let index = 0; index < lis.length; index += 1) {
+        arrayLis.push(lis[index].innerText);
+      }
+      localStorage.setItem('cart', JSON.stringify(arrayLis));
     }
-    localStorage.setItem('cart', JSON.stringify(arrayLis));
   }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
   event.remove();
   saveLocStorCart();
 }
@@ -37,8 +42,7 @@ function createCartItemElement({ id, title = 0, price }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
   
-  const ol = document.querySelector('.cart__items');
-  ol.appendChild(li);  
+  oL().appendChild(li);  
 
   li.addEventListener('click', (event) => {
     cartItemClickListener(event.target);
@@ -49,23 +53,13 @@ function createCartItemElement({ id, title = 0, price }) {
 
 function inserirLocStorCart() {
   const jsonLoc = JSON.parse(localStorage.getItem('cart'));
-  
-  const ol = document.querySelector('.cart__items'); 
-  if (jsonLoc !== null && jsonLoc.length > 0) {
+  if (jsonLoc !== null) {
   for (let index = 0; index < jsonLoc.length; index += 1) {
-    const criaElement = createCustomElement('li', 'cart__item', `${jsonLoc[index]}`);
-    ol.appendChild(criaElement);
+   const criaElement = createCustomElement('li', 'cart__item', `${jsonLoc[index]}`);
+   oL().appendChild(criaElement);
   }
   }
 }
-
-// btn();
-// function btn() {
-//   const btn = document.querySelector('.empty-cart')
-//   btn.addEventListener('click', (event) => {
-//     console.log((event.target))
-//   })
-// }
 
 function requestItemSelect(id) { // Requisita o item selecionado através do id 
   fetch(`https://api.mercadolibre.com/items/${id}`)
@@ -112,7 +106,15 @@ function promessa(event) {
   .catch((erro) => console.log(erro));
 }
 
+function btn () {
+  const btn = document.querySelector('.empty-cart');
+  btn.addEventListener('click', () => {
+    oL().innerText = '';
+  });
+}
+
 window.onload = () => { 
   promessa('computador');
   inserirLocStorCart();
+  btn();
 };
